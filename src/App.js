1827486f3a90c096d237524d310fbb06e7286ef6
SarahPruene <sarah.pruene91@gmail.com>
2020-11-30 10:50:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Weather from './Weather'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <Weather defaultCity='London'/>
+        <footer>
+          This project was coded by <span>Sarah</span> and is{' '}
+          <a href='/' target='_blank'>
+            open-sourced on Github
+          </a>{' '}
+          <br />
+          and{' '}
+          <a href='/' target='_blank'>
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/ForecastBox.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import WeatherIcon from './WeatherIcon'
+
+import './Forecast.css'
+
+export default function ForcastBox(props) {
+  function hours() {
+    let date = new Date(props.data.dt * 1000)
+    let hours = date.getHours()
+
+    return `${hours}:00`
+  }
+
+  function minTemp() {
+    let minTemperature = Math.round(props.data.main.temp_min)
+
+    return `${minTemperature}°C`
+  }
+
+  function maxTemp() {
+    let maxTemperature = Math.round(props.data.main.temp_max)
+
+    return `${maxTemperature}°C`
+  }
+  
+  return (
+    <div className='ForecastBox'>
+      <h3 className='forecadast-title'>{hours()}</h3>
+      <WeatherIcon code={props.data.weather[0].icon} />
+      <div className='forecast-temp'>
+        <strong>{minTemp()}</strong> | {maxTemp()}
+      </div>
+    </div>
+  )
+}
